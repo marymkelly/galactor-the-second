8164413ce3d67bb177d0e6b9d55f8f883e74e13c
Mary Kelly <mk-km@live.com>
2021-02-06 17:07:31 -0500

    add firebase dependencies and update files

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 
+import { firebase } from './firebase/firebase';
+
 //express 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.get('/', function (req, res) {
+	firebase.analytics()
 	res.render('index');
 });
 
 app.get('/stars', (req, res) => {
 	res.render('stars');
 });
+
+// firebase.analytics();
 
 module.exports = app;
